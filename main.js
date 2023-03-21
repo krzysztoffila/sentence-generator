955b5287a482b6a1b0b5828da409ad73a4e485f7
krzysztoffila <umowy.fila@gmail.com>
2023-03-21 15:24:10 +0100

    TODO showAdvice

---
+++ main.js
@@ -0,0 +1,29 @@
+let sentences = ['walcz', 'nie poddawaj sie'];
+const input = document.querySelector('input');
+
+const addSentence = event => {
+    event.preventDefault();
+    sentence = input.value;
+    sentences.push(sentence);
+    console.log(sentences);
+}
+const cleanSentences = event => {
+    event.preventDefault();
+    sentences = [];
+    console.log(sentences);
+}
+const showAdvice = event => {
+    event.preventDefault();
+    const h1 = document.querySelector('h1');
+
+}
+const showOptions = event => {
+    event.preventDefault();
+    alert(sentences);
+}
+
+
+document.querySelector('.add').addEventListener('click', addSentence)
+document.querySelector('.clean').addEventListener('click', cleanSentences)
+document.querySelector('.showAdvice').addEventListener('click', showAdvice)
+document.querySelector('.showOptions').addEventListener('click', showOptions)
